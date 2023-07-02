068bf2e2b8f58a743b8fff4baf50aeeb06dc822b
suzoagba <samuzoagba@gmail.coma>
2023-07-02 15:48:18 +0300

    forum authentication

--- oauth/clientInfo.go
+++ oauth/clientInfo.go
@@ -1,10 +1,12 @@
 package oauth
 
 const (
-	GoogleClientID     = ""
-	GoogleClientSecret = ""
+	GoogleClientID     = "269145944343-dmr9dl9uast2vd62m6j1b33cj08nkecb.apps.googleusercontent.com"
+	GoogleClientSecret = "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"
 	GoogleRedirectURI  = "http://localhost:8080/oauth/google"
-	GithubClientID     = ""
-	GithubClientSecret = ""
+	GithubClientID     = "0a5692f8fc07ad93a8b3"
+	GithubClientSecret = "373b5e1ff7f6a6e88502c3cb8a411b40e7fd5726"
 	GithubRedirectURI  = "http://localhost:8080/oauth/github"
 )
+
+// {"web":{"client_id":"269145944343-dmr9dl9uast2vd62m6j1b33cj08nkecb.apps.googleusercontent.com","project_id":"forum-authentication-391612","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost:8080/oauth/google"],"javascript_origins":["http://localhost:8080"]}}
