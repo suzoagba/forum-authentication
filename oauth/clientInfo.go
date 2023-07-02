562071c2e94a5c33266dda7f8a5291b2cd9e2365
suzoagba <samuzoagba@gmail.coma>
2023-07-02 18:54:08 +0300

    forum authentication

--- oauth/clientInfo.go
+++ oauth/clientInfo.go
@@ -8,5 +8,3 @@ const (
 	GithubClientSecret = ""
 	GithubRedirectURI  = "http://localhost:8080/oauth/github"
 )
-
-// {"web":{"client_id":"269145944343-dmr9dl9uast2vd62m6j1b33cj08nkecb.apps.googleusercontent.com","project_id":"forum-authentication-391612","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost:8080/oauth/google"],"javascript_origins":["http://localhost:8080"]}}
