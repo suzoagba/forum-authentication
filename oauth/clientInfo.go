f0645cf6f5c1749569388b46f59e45256d11f802
suzoagba <samuzoagba@gmail.coma>
2023-07-02 17:55:50 +0300

    forum authentication

--- oauth/clientInfo.go
+++ oauth/clientInfo.go
@@ -1,11 +1,11 @@
 package oauth
 
 const (
-	GoogleClientID     = "269145944343-dmr9dl9uast2vd62m6j1b33cj08nkecb.apps.googleusercontent.com"
-	GoogleClientSecret = "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"
+	GoogleClientID     = ""
+	GoogleClientSecret = ""
 	GoogleRedirectURI  = "http://localhost:8080/oauth/google"
-	GithubClientID     = "0a5692f8fc07ad93a8b3"
-	GithubClientSecret = "373b5e1ff7f6a6e88502c3cb8a411b40e7fd5726"
+	GithubClientID     = ""
+	GithubClientSecret = ""
 	GithubRedirectURI  = "http://localhost:8080/oauth/github"
 )
 
